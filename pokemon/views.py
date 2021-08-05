7d29f5e77ee295563a4e483f8e6a2a0e267aaf89
Artur Rejment <arturrejment1@gmail.com>
2021-08-06 00:12:18 +0200

    Displaying crucial informations for main page

--- pokemon/views.py
+++ pokemon/views.py
@@ -9,5 +9,22 @@ class MainPageView(TemplateView):
 
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
-		context['pokemon_data'] = requests.get('https://pokeapi.co/api/v2/pokemon/').json()['results']
+		page = self.request.GET.get('page', 1)
+		offset = (int(page) * 10 - 10)
+		payload = {'limit':10, 'offset': offset}
+		pokemons_data = requests.get('https://pokeapi.co/api/v2/pokemon/', params=payload).json()['results']
+		data = []
+		for pokemon in pokemons_data:
+			dictionary = {}
+			response = requests.get(pokemon['url']).json()
+			dictionary['name'] = response['name']
+			dictionary['id'] = response['id']
+			types = []
+			for type in response['types']:
+				types.append(type['type']['name'])
+			dictionary['types'] = types
+			data.append(dictionary)
+			# print(data)
+		print(data)
+		context['pokemon_data'] = data
 		return context
