6c155ab3ff3a58cca50c0ec5d68f91f6d4ac1fef
Artur Rejment <arturrejment1@gmail.com>
2021-08-08 03:24:18 +0200

    Login and logout templates sytled

--- authentication/forms.py
+++ authentication/forms.py
@@ -7,11 +7,18 @@ class RegisterForm(UserCreationForm):
 	""" Inherite from UserCreationForm in order to add
 	new field "email", because UserCreationFrom does not
 	support this field. """
-	email = forms.EmailField(label = "Email")
+	email = forms.EmailField(label="Email")
 
 	class Meta:
 		model = User
 		fields = ("username", "email",)
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields['username'].widget.attrs.update({'placeholder': 'username',})
+		self.fields['email'].widget.attrs.update({'placeholder': 'email',})
+		self.fields['password1'].widget.attrs.update({'placeholder': 'password',})
+		self.fields['password2'].widget.attrs.update({'placeholder': 're-password',})
 
 	def save(self, commit=True):
 		""" Override save function to save also email """
