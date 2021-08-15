6484b3248f862720bcb7224ea30655a381f866c5
Artur Rejment <arturrejment1@gmail.com>
2021-08-15 16:08:36 +0200

    Moved Pokemon model to Pokemon application

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Pokemon(models.Model):
+	""" Pokemon model to store """
+	pokemon_id = models.PositiveIntegerField(
+		primary_key=True, unique=True,
+		null=False, blank=False
+	)
