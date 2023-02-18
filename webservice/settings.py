48c68aaf222bd7402a2c2000b72d5b4caea22dc2
arturrejment <arturrejment1@gmail.com>
2023-02-18 14:43:39 +0100

    set django debug variable, restore static files

--- webservice/settings.py
+++ webservice/settings.py
@@ -133,7 +133,9 @@ AUTH_USER_MODEL = 'authentication.User'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static/'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
